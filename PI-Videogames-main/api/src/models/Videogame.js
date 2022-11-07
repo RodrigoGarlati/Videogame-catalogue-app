19cad69cfc14dd63ee89bea472c88499c003ef11
unknown <rodrigarlati@gmail.com>
2022-11-07 12:45:28 -0300

    db changes

--- PI-Videogames-main/api/src/models/Videogame.js
+++ PI-Videogames-main/api/src/models/Videogame.js
@@ -4,9 +4,29 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id : {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    releaseDate: {
+      type: DataTypes.DATEONLY,
+    },
+    rating: {
+      type: DataTypes.INTEGER
+    },
+    platforms: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+
   });
 };
