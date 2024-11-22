25d8a2691e5b17e494c4ebfd1f1ffbe63a494210
Rodrigo Garlati <rodrigarlati@gmail.com>
2024-11-22 08:03:29 -0300

    add unit testing and adapt endpoints to ut

--- PI-Videogames-main/api/src/routes/videogameRouter.js
+++ PI-Videogames-main/api/src/routes/videogameRouter.js
@@ -1,19 +1,18 @@
 const {Router} = require('express')
 const router = Router()
-const axios = require('axios')
-const { getGamesFromDb, getCountGamesFromDb } = require('../common/getGamesFromDb')
-const { getGamesFromApi, getApiGameById } = require('../common/getGamesFromApi')
+const dbUtils = require('../common/getGamesFromDb')
+const apiUtils = require('../common/getGamesFromApi')
 const { PAGES_FOR_API_COUNT_LIMIT } = require('../common/constants')
 
 router.get('/all', async(req, res)=>{
     const {game_name, page} = req.query
     try {
-        const dbGamesAmount = await getCountGamesFromDb(game_name)
+        const dbGamesAmount = await dbUtils.getCountGamesFromDb(game_name)
         let games = []
         let count = dbGamesAmount
-        const dbGames = page >= PAGES_FOR_API_COUNT_LIMIT ? await getGamesFromDb(game_name, page) : null
+        const dbGames = page >= PAGES_FOR_API_COUNT_LIMIT ? await dbUtils.getGamesFromDb(game_name, page) : null
         const justForCount = page > PAGES_FOR_API_COUNT_LIMIT
-        const apiGames = await getGamesFromApi(game_name, page, justForCount)
+        const apiGames = await apiUtils.getGamesFromApi(game_name, page, justForCount)
         count += apiGames.count
         if (page == PAGES_FOR_API_COUNT_LIMIT){
             games = [...apiGames.games, ...dbGames.games]
@@ -38,7 +37,7 @@ router.get('/all', async(req, res)=>{
 router.get('/api', async(req, res) => {
     const {gameName, page} = req.query
     try {
-        const apiGames = await getGamesFromApi(gameName, page)
+        const apiGames = await apiUtils.getGamesFromApi(gameName, page)
         res.json(apiGames)
     } catch (error) {
         res.status(500).json(error)
@@ -48,7 +47,7 @@ router.get('/api', async(req, res) => {
 router.get('/api/:id', async (req, res)=>{
     try{
         const {id} = req.params
-        const game = await getApiGameById(id)
+        const game = await apiUtils.getApiGameById(id)
         res.json(game)
     }
     catch(error){
--- PI-Videogames-main/api/tests/routes/videogame.test.js
+++ PI-Videogames-main/api/tests/routes/videogame.test.js
@@ -2,31 +2,107 @@ const app = require('../../src/app')
 const utilsDb = require('../../src/common/getGamesFromDb')
 const utilsApi = require('../../src/common/getGamesFromApi')
 const request = require('supertest')
+const { Videogame } = require('../../src/db')
+const TEST_MAX_TIMEOUT = 20000
 
-// describe('Test get all games', () => {
-//   test('It should obtain all games', () => {
+describe('Testing get all games:', () => {
+  const spyOnGetDbGames = jest.spyOn(utilsDb, 'getGamesFromDb')
+  const spyOnGetCountDbGames = jest.spyOn(utilsDb, 'getCountGamesFromDb')
+  const spyOnGetApiGames = jest.spyOn(utilsApi, 'getGamesFromApi')
 
-//   })
-// })
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+  
+  test('It should obtain all games', async () => {
+    await request(app).get('/videogames/all?page=7')
 
-describe('Testing get all created games', () => {
+    expect(spyOnGetCountDbGames).toBeCalled()
+    expect(spyOnGetDbGames).toBeCalled()
+    expect(spyOnGetApiGames).toBeCalled()
+  }, TEST_MAX_TIMEOUT)
+
+  test('It receives necesary data for obtaining all games', async () => {
+    await request(app).get('/videogames/all?page=7')
+
+    expect(spyOnGetCountDbGames).toHaveBeenCalledWith(undefined)
+    expect(spyOnGetDbGames).toHaveBeenCalledWith(undefined, '7')
+    expect(spyOnGetApiGames).toHaveBeenCalledWith(undefined, '7', false)
+  }, TEST_MAX_TIMEOUT)
+
+  test('It paginates correctly based on the current page', async () => {
+    await request(app).get('/videogames/all?page=1')
+
+    expect(spyOnGetCountDbGames).toBeCalledTimes(1)
+    expect(spyOnGetApiGames).toBeCalledTimes(1)
+    expect(spyOnGetApiGames).toHaveBeenCalledWith(undefined, '1', false)
+
+    await request(app).get('/videogames/all?page=8')
+
+    expect(spyOnGetCountDbGames).toBeCalledTimes(2)
+    expect(spyOnGetDbGames).toBeCalledTimes(1)
+    expect(spyOnGetApiGames).toBeCalledTimes(2)
+    expect(spyOnGetApiGames).toHaveBeenCalledWith(undefined, '8', true)
+  }, TEST_MAX_TIMEOUT)
+
+  test('It search correctly by game name', async () => {
+    await request(app).get('/videogames/all?page=7&game_name=grand')
+
+    expect(spyOnGetDbGames).toHaveBeenCalledWith('grand', '7')
+    expect(spyOnGetCountDbGames).toHaveBeenCalledWith('grand')
+    expect(spyOnGetApiGames).toHaveBeenCalledWith('grand', '7', false)
+  }, TEST_MAX_TIMEOUT)
+})
+
+describe('Testing get created games:', () => {
+  const spyOnGetGamesUtils = jest.spyOn(utilsDb, 'getGamesFromDb')
+  const spyOnGetCountGames = jest.spyOn(utilsDb, 'getCountGamesFromDb')
+  const spyOnGetDbGameById = jest.spyOn(Videogame, 'findByPk');
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
   test('It should obtain and count db videogames', async () => {
-    const spyOnGetGamesUtils = jest.spyOn(utilsDb, 'getGamesFromDb')
-    const spyOnGetCountGames = jest.spyOn(utilsDb, 'getCountGamesFromDb')
-
     await request(app).get('/db/videogames?page=1')
 
     expect(spyOnGetGamesUtils).toBeCalled()
     expect(spyOnGetCountGames).toBeCalled()
-  })
+  }, TEST_MAX_TIMEOUT)
   
   test('It receives necesary data for obtaining games', async () => {
-    const spyOnGetGamesUtils = jest.spyOn(utilsDb, 'getGamesFromDb')
-
     await request(app).get('/db/videogames?page=1')
     await request(app).get('/db/videogames?page=2')
 
     expect(spyOnGetGamesUtils).toHaveBeenCalledWith(undefined, '1', true)
     expect(spyOnGetGamesUtils).toHaveBeenCalledWith(undefined, '2', true)
+  }, TEST_MAX_TIMEOUT)
+
+  test('It correctly obtain a created game by ID', async () => {
+    await request(app).get('/db/videogames/1')
+
+    expect(spyOnGetDbGameById).toHaveBeenCalledWith('1')
+  }, TEST_MAX_TIMEOUT)
+})
+
+describe('Testing get api games:', () => {
+  const spyOnGetApiGames = jest.spyOn(utilsApi, 'getGamesFromApi')
+  const spyOnGetApiGameById = jest.spyOn(utilsApi, 'getApiGameById')
+  
+  afterEach(() => {
+    jest.clearAllMocks()
   })
+
+  test('It correctly call get api games method', async ()=>{
+    await request(app).get('/videogames/api?page=8')
+    
+    expect(spyOnGetApiGames).toBeCalled()
+    expect(spyOnGetApiGames).toHaveBeenCalledWith(undefined, '8')
+  }, TEST_MAX_TIMEOUT)
+
+  test('It correctly obtain an api game by ID', async () => {
+    await request(app).get('/videogames/api/1234')
+    
+    expect(spyOnGetApiGameById).toHaveBeenCalledWith('1234')
+  }, TEST_MAX_TIMEOUT)
 })
